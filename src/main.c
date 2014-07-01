6b659bfb07895f166ba9dc5c10dfcb1b58210a96
Simon Diemert <simon.diemert@gmail.com>
2014-07-01 14:49:18 -0700

    added rsa for the modular exp and lookup table methods, cleaned up outputs

--- src/main.c
+++ src/main.c
@@ -72,6 +72,36 @@ int modular_exp(int base, int exp, int divsor){
         return result; 
 }
 
+/*
+ * This assumes that we need to compute the lookup
+ * tables for each new call to encrpy. 
+ */
+int rsa_encrpyt_lookup_table(int data, int m, int public_key){
+        int powers[MAX_TABLE_SIZE]; 
+        int lookup_table[MAX_TABLE_SIZE]; 
+        int num_powers = compute_powers_of_two(public_key, 32, powers);
+        int table_size = compute_lookup_table(data, m, powers, num_powers, lookup_table); 
+        int x = compute_modulus_from_look_up_table(lookup_table, num_powers, powers, m); 
+        return x;  
+}
+
+int rsa_decrypt_lookup_table(int data, int m, int private_key){
+        int powers[MAX_TABLE_SIZE]; 
+        int lookup_table[MAX_TABLE_SIZE]; 
+        int num_powers = compute_powers_of_two(private_key, 32, powers);
+        int table_size = compute_lookup_table(data, m, powers, num_powers, lookup_table); 
+        int x = compute_modulus_from_look_up_table(lookup_table, num_powers, powers, m); 
+        return x;  
+}
+
+int rsa_encrypt_modular_exp(int data, int m, int public_key){
+       return modular_exp(data, public_key, m);  
+}
+
+int rsa_decrypt_modular_exp(int data, int m, int private_key){
+       return modular_exp(data, private_key, m);  
+}
+
 int main(int argc, char * argv[]){
         
         int r[MAX_TABLE_SIZE]; 
@@ -89,8 +119,37 @@ int main(int argc, char * argv[]){
 
         int x = compute_modulus_from_look_up_table(table, count, r, divsor_value); 
 
+        printf("=========UNIT TESTS========\n"); 
         printf("Modular Exp Result: %d\n", mod_exp); 
         printf("Table Result : %d\n", x); 
+
+        printf("\n\n=========RSA TESTS========\n"); 
+        printf("Inputs: \n"); 
+        
+        int public_key = 17; 
+        int private_key = 2753; 
+        int divsor = 3233; 
+        int plain_text = 123; 
+        int cipher_text = 0; 
+        int new_text = 0; 
+
+        printf("public_key : %d\n", public_key); 
+        printf("private_key : %d\n", private_key); 
+        printf("divsor : %d\n", divsor); 
+        printf("plain_text : %d\n", plain_text); 
+
+        //Test the lookup table method of encryption. This will recompute the 
+        //lookup table for each call to the encrpyt/decrypt functions. 
+        printf("\n---LOOKUP TABLE METHOD-----\n"); 
+        cipher_text = rsa_encrpyt_lookup_table(plain_text, divsor, public_key); 
+        new_text = rsa_decrypt_lookup_table(cipher_text, divsor, private_key); 
+        printf("cipher_text : %d, plain_text : %d\n", cipher_text, new_text); 
+
+        //Tests the modular exponetation method of encryption.
+        printf("\n---MODULAR EXPONENATION METHOD-----\n"); 
+        cipher_text = rsa_encrypt_modular_exp(plain_text, divsor, public_key); 
+        new_text = rsa_decrypt_modular_exp(cipher_text, divsor, private_key); 
+        printf("cipher_text : %d, plain_text : %d\n", cipher_text, new_text); 
 
         return 0; 
 }
